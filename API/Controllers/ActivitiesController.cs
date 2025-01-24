10107e7abb204066e682265a6bd81adc2c88a015
tarikemir <81103446+tarikemir@users.noreply.github.com>
2025-01-24 23:28:25 +0300

    CRUD Applicaiton using CQRS

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -1,26 +1,56 @@
+using Application.Activities;
 using Domain;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using Persistence;
 
 namespace API.Controllers;
 
 public class ActivitiesController : BaseApiController
 {
-    private readonly DataContext _context;
-    public ActivitiesController( DataContext context){
-        _context = context;
-    }
 
-    [HttpGet] //api/activities
+    [HttpGet]
     public async Task<ActionResult<List<Activity>>> GetActivities()
     {
-        return await _context.Activities.ToListAsync();
+        return await Mediator.Send( new List.Query());
     }
 
     [HttpGet("{id}")]
     public async Task<ActionResult<Activity>> GetActivity( Guid id)
     {
-        return await _context.Activities.FirstOrDefaultAsync( x => x.Id == id);
+        return await Mediator.Send( new Details.Query{
+            Id = id
+        });
     }
+
+    [HttpPost]
+    public async Task<IActionResult> CreateActivity( Activity activity){
+        
+        await Mediator.Send( new Create.Command(){
+            Activity = activity
+        });
+
+        return Ok();
+    }
+
+    [HttpPut("{id}")]
+    public async Task<IActionResult> EditActivity( Guid id, Activity activity){
+
+        activity.Id = id;
+
+        await Mediator.Send( new Edit.Command(){
+            Activity = activity
+        });
+
+        return Ok();
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteActivity( Guid Id){
+        
+        await Mediator.Send( new Delete.Command(){
+            Id = Id
+        });
+
+        return Ok();
+    }
+
 }
--- API/Program.cs
+++ API/Program.cs
@@ -1,3 +1,6 @@
+using API.Extensions;
+using Application.Activities;
+using Application.Core;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
 
@@ -6,18 +9,9 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
-builder.Services.AddDbContext<DataContext>(opt => 
-{
-    opt.UseSqlite( builder.Configuration.GetConnectionString("DefaultConnection"));
-});
+builder.Services.AddApplicationServices( builder.Configuration);
 
-builder.Services.AddCors( opt => {
-    opt.AddPolicy("CorsPolicy", policy => {
-        policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
-    });
-});
+builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
