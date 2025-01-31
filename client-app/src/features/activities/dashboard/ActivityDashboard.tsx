fcdb76f2d23a99e28e69a2839d59333906fd36ca
tarikemir <81103446+tarikemir@users.noreply.github.com>
2025-01-31 16:11:45 +0300

    CRUD api integration

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -14,15 +14,19 @@ interface Props {
     closeForm: () => void;
     createOrEdit: (activity : Activity) => void;
     deleteActivity: ( id: string) => void;
+    submitting: boolean;
 }
 
-export default function ActivityDashboard({ activities, selectedActivity, selectActivity, cancelSelectedActivity, editMode, openForm, closeForm, createOrEdit, deleteActivity }: Props) {
+export default function ActivityDashboard({ activities, selectedActivity, selectActivity, cancelSelectedActivity, 
+    editMode, openForm, closeForm, createOrEdit, deleteActivity, submitting }: Props) {
+
     return (
         <Grid>
             <Grid.Column width={"10"}>
                 <ActivityList activities={activities}
                     selectActivity={selectActivity}
                     deleteActivity={deleteActivity}
+                    submitting={submitting}
                 />
             </Grid.Column>
             <Grid.Column width={"6"}>
@@ -33,7 +37,7 @@ export default function ActivityDashboard({ activities, selectedActivity, select
                         openForm={openForm}
                         />}
                 {editMode && 
-                    <ActivityForm closeForm={closeForm} activity={selectedActivity} createOrEdit={createOrEdit}/>
+                    <ActivityForm closeForm={closeForm} activity={selectedActivity} createOrEdit={createOrEdit} submitting={submitting}/>
                     }
             </Grid.Column>
         </Grid>
--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -6,8 +6,9 @@ interface Props {
     activity: Activity | undefined;
     closeForm: () => void;
     createOrEdit: ( activity: Activity) => void;
+    submitting: boolean;
 }
-export default function ActivityForm({ activity: selectedActivity, closeForm, createOrEdit}: Props) {
+export default function ActivityForm({ activity: selectedActivity, closeForm, createOrEdit, submitting}: Props) {
 
     const initialState = selectedActivity ?? {
         id: "",
@@ -37,10 +38,10 @@ export default function ActivityForm({ activity: selectedActivity, closeForm, cr
                 <Form.Input placeHolder="Title" value={activity.title} name="title" onChange={handleInputChange}/>
                 <Form.TextArea placeHolder="Description" value={activity.description} name="description" onChange={handleInputChange}/>
                 <Form.Input placeHolder="Category" value={activity.category} name="category" onChange={handleInputChange}/>
-                <Form.Input placeHolder="Date" value={activity.date} name="date" onChange={handleInputChange}/>
+                <Form.Input type="date" placeHolder="Date" value={activity.date} name="date" onChange={handleInputChange}/>
                 <Form.Input placeHolder="City" value={activity.city} name="city" onChange={handleInputChange}/>
                 <Form.Input placeHolder="Venue" value={activity.venue} name="venue" onChange={handleInputChange}/>
-                <Button floated="right" positive type="submit" content="Submit" />
+                <Button loading={submitting} floated="right" positive type="submit" content="Submit" />
                 <Button onClick={() => closeForm()} floated="right" type="button" content="Cancel" />
             </Form>
         </Segment>
